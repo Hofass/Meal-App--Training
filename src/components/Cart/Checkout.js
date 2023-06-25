45b3e248eb459acce709aff12de587c187b49608
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-06-25 16:37:25 +0200

    Adding form

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,13 @@
+import classes from "./Checkout.module.css"
+
+
+const Checkout = ()=>{
+  return <form>
+    <div className={classes.control}>
+      
+    </div>
+  </form>
+};
+
+
+export default Checkout;
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -15,16 +15,11 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById('overlays');
 
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
-      {ReactDOM.createPortal(
-        <ModalOverlay>{props.children}</ModalOverlay>,
-        portalElement
-      )}
+        <ModalOverlay>{props.children}</ModalOverlay>
     </Fragment>
   );
 };
