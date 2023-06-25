79bfc66bdf6509bd73afc3a4d327bc1adacf13ef
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-06-25 10:11:31 +0200

    Fetching data part2

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,34 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
-
-
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
+	useEffect(() => {
+		const fetchMeals = async () => {
+			const response = await fetch(
+				"https://training-2d545-default-rtdb.firebaseio.com/meals.json"
+			);
+      const responseData = await response.json();
 
-  useEffect(()=>{
-    fetch('https://training-2d545-default-rtdb.firebaseio.com/meals.json').then(()=>{})
-  },[])
+      const loadedMeals = []
+      for(const key in responseData){
+        loadedMeals.push({
+          id:key,
+          name:responseData[key].name,
+          description:responseData[key].description,
+          price:responseData[key].price
+        })
+      }
+		};
 
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+		fetchMeals();
+	}, []);
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+	const mealsList = DUMMY_MEALS.map(meal => (
+		<MealItem
+			key={meal.id}
+			id={meal.id}
+			name={meal.name}
+			description={meal.description}
+			price={meal.price}
+		/>
+	));
+
+	return (
+		<section className={classes.meals}>
+			<Card>
+				<ul>{mealsList}</ul>
+			</Card>
+		</section>
+	);
 };
 
 export default AvailableMeals;
